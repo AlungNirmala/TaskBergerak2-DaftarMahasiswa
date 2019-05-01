6724f3e8484d06dcff2ade2d28594f231444fb29
AlungNirmala <alung.nirmala@gmail.com>
2019-05-02 06:48:25 +0800

    upload pertama

---
+++ app/src/main/java/com/example/hp/taskbergerak2_f1d016014/Coba.java
@@ -0,0 +1,13 @@
+package com.example.hp.taskbergerak2_f1d016014;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Coba extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_coba);
+    }
+}
